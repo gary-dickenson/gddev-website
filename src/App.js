d61a50ee9e7e84820b970d3337e42f6058ab3f07
Gary <gary@gddevltd.co.uk>
2020-06-18 12:46:22 +0100

    fix build

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import React, { Component } from 'react'
-import Home from './components/Home'
-import './App.css'
 import React, { Component } from 'react';
 import Home from './components/Home';
+import './App.css';
 import './styles/App.scss';
 import Header from './components/Header';
 import Footer from './components/Footer';
