9b937cdb53a6337516536809525e6d3c2e2ada8d
Gary <info@gddevltd.co.uk>
2020-06-22 18:53:22 +0100

    Improvements to Skills component

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,36 @@
 const path = require('path')
 // const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
-  plugins: [
+  entry: './src/index.js', devtool: 'inline-source-map', plugins: [
     // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
     // new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Development',
-      template: './public/index.html',
-    }),
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
+      title: 'Development', template: './public/index.html'
+    }), new CopyPlugin({
+      patterns: [
+        { from: 'assets', to: 'assets' }]
+    })], output: {
+    filename: '[name].bundle.js', path: path.resolve(__dirname, 'dist')
+  }, devServer: {
     // contentBase: './public'
-  },
-  module: {
+  }, module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader'],
-      },
-      {
-        test: /\.css$/i,
-        exclude: /node_modules/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(s[ac]ss|min.css)$/i,
-        use: [
+        test: /\.(js|jsx)$/, exclude: /node_modules/, use: ['babel-loader', 'eslint-loader']
+      }, {
+        test: /\.css$/i, exclude: /node_modules/, use: ['style-loader', 'css-loader']
+      }, {
+        test: /\.(s[ac]ss|min.css)$/i, use: [
           // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|gif|ico)$/,
-        exclude: /node_modules/,
-        use: [
-          'file-loader',
-        ],
-      },
-    ],
-  },
+          'style-loader', // Translates CSS into CommonJS
+          'css-loader', // Compiles Sass to CSS
+          'sass-loader']
+      }, {
+        test: /\.(svg|ico)$/, exclude: /node_modules/, use: [
+          'url-loader']
+      }]
+  }
 }
